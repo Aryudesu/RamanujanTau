0b9dc9b5f290688868e6530caf08da0ce794e4f3
Aryudesu <suwako_a-u-@hotmail.co.jp>
2023-09-10 22:16:08 +0900

    とりあえず作成

---
+++ main.py
@@ -0,0 +1,43 @@
+def calc_pascal_triangle(num):
+    dp = [1]
+    for i in range(num):
+        new_dp = []
+        s = 0
+        bs = 0
+        for j in dp:
+            s = bs + j
+            bs = j
+            new_dp.append(s)
+        new_dp.append(1)
+        dp = new_dp
+    return dp
+
+
+def calc(num):
+    ta = calc_pascal_triangle(24)
+    lta = len(ta)
+    dp = {0: 1}
+    n = 1
+    for n in range(1, num + 1):
+        new_dp = dict()
+        for d in dp:
+            sgn = 1
+            for idx in range(lta):
+                key = d + n * idx
+                nndp = new_dp.get(key, 0)
+                tmp = dp.get(d) * (sgn * ta[idx])
+                new_dp[key] = nndp + tmp
+                sgn = -sgn
+        dp = new_dp
+    result = [0]
+    for i in range(num):
+        result.append(dp[i])
+    return result
+
+
+def inputNum():
+    print("InputNum > ", end="")
+    return int(input())
+
+
+print(calc(inputNum()))
